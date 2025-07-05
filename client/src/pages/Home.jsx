8876e1f6cc401e325f8d36b43d33ea087fa8fe89
Aniketu28 <uagreaniket28@gmail.com>
2025-07-05 22:49:43 +0530

    router and context setup done

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React, { useContext } from 'react'
+import { AppContext } from '../context/AppContext'
+
+const Home = () => {  
+
+  return (
+    <div>
+      home 
+    </div>
+  )
+}
+
+export default Home
