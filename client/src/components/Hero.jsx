2f18c26e53e543003a54a486f3f2d06e6e4b2a21
Aniketu28 <uagreaniket28@gmail.com>
2025-07-09 22:47:24 +0530

    resolve responsive issues

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -20,12 +20,12 @@ const Hero = () => {
     }
 
     return (
-        <div className='container mx-auto 2xl:px-20 my-10'>
-            <div className='bg-gradient-to-r from-[#4F0487] to-[#130121] py-16 text-white text-center rounded-xl'>
+        <div className='container px-3 mx-auto 2xl:px-20 my-10'>
+            <div className='bg-gradient-to-r from-[#4F0487] to-[#130121] py-16 text-white text-center rounded-xl max-sm:px-2'>
                 <h2 className='text-3xl md:text-3xl lg:text-4xl font-medium mb-4'>Over 10,000+ jobs to apply</h2>
                 <p className='mb-8 max-w-xl mx-auto text-[15px] font-light px-5'>Your Next Big Career Move Starts Right Here - Explore the Best Job Opportunities and Take the First Step Toward Your Future!</p>
-                <div className='flex max-sm:flex-col items-center justify-between bg-white rounded text-gray-600 max-w-xl px-4 mx-4 sm:mx-auto'>
-                    <div className='flex items-center'>
+                <div className='flex max-sm:flex-col items-center justify-between bg-white rounded text-gray-600 max-w-xl px-4 max-sm:py-4 mx-4 sm:mx-auto max-sm:gap-2'>
+                    <div className='flex items-center max-sm:w-full'>
                         <img className='h-4 sm:h-5' src={assets.search_icon} alt="search-icon" />
                         <input type="text"
                             placeholder='search for jobs'
@@ -33,7 +33,7 @@ const Hero = () => {
                             ref={titleRef}
                         />
                     </div>
-                    <div className='flex items-center'>
+                    <div className='flex items-center max-sm:w-full'>
                         <img className='h-4 sm:h-5 mr-1' src={assets.location_icon} alt="location-icon" />
                         <input type="text"
                             placeholder='location'
@@ -41,10 +41,10 @@ const Hero = () => {
                             ref={locationRef}
                         />
                     </div>
-                    <button onClick={onSearch} className='bg-blue-600 px-8 py-2 rounded text-white my-2'>Search</button>
+                    <button onClick={onSearch} className='bg-blue-600 px-8 py-2 rounded text-white my-2 max-sm:w-full'>Search</button>
                 </div>
             </div>
-            <div className='border border-gray-300 mt-12 p-6 rounded-md flex'>
+            <div className='border border-gray-300 mt-12 max-sm:px-3 max-sm:py-5 p-6 rounded-md flex'>
                 <div className='flex justify-center items-center gap-10 lg:gap-16 flex-wrap'>
                     <p className='font-medium'>Trusted by</p>
                     <img className='h-6' src={assets.microsoft_logo} alt="Company Logo" />
--- client/src/components/Joblist.jsx
+++ client/src/components/Joblist.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import { AppContext } from '../context/AppContext'
 import { assets, JobLocations, jobsData } from '../assets/assets';
 import { JobCategories } from '../assets/assets'
@@ -6,7 +6,8 @@ import JobCard from './JobCard';
 
 const Joblist = () => {
 
-    const { searchFilter, setSearchFilter,jobsData } = useContext(AppContext);
+    const { searchFilter, setSearchFilter, jobsData } = useContext(AppContext);
+    const [showFilters, setShowFilters] = useState(false);
 
     return (
         <div className='container 2xl:px-20 mx-auto flex flex-col lg:flex-row max-lg:space-y-8 min-lg:py-6'>
@@ -41,14 +42,18 @@ const Joblist = () => {
                     </>
                 )}
 
+                <button onClick={()=>{setShowFilters((prev)=> !prev)}} className='px-4 py-2 mt-4 text-gray-600 shadow border border-gray-200 min-lg:hidden'>
+                    {showFilters ? 'close' : 'filters'}
+                </button>
+
                 {/*categories filter */}
-                <div className='max-lg:hidden mt-8'>
+                <div className={`${showFilters ? '' : 'max-lg:hidden'} mt-8`}>
                     <h3 className='font-medium text-lg mb-4'>Search by Categories</h3>
-                    <ul className='space-y-4 text-gray-600'>
+                    <ul className='space-y-4 text-gray-600 max-lg:flex max-lg:flex-row max-lg:gap-4 max-lg:flex-wrap max-lg:mt-2'>
                         {
                             JobCategories.map((category, index) =>
                             (
-                                <li key={index} className='flex gap-3 items-center'>
+                                <li key={index} className='flex gap-3 items-center max-lg:mb-1'>
                                     <input className='scale-125' type="checkbox" name='' id='' />
                                     {category}
                                 </li>
@@ -59,13 +64,13 @@ const Joblist = () => {
                 </div>
 
                 {/*location filter */}
-                <div className='max-lg:hidden mt-8'>
+                <div className={`${showFilters ? '' : 'max-lg:hidden'} mt-8`}>
                     <h3 className='font-medium text-lg mb-4'>Search by Locations</h3>
-                    <ul className='space-y-4 text-gray-600'>
+                    <ul className='space-y-4 text-gray-600 max-lg:flex max-lg:flex-row max-lg:gap-4 max-lg:flex-wrap max-lg:mt-2'>
                         {
                             JobLocations.map((category, index) =>
                             (
-                                <li key={index} className='flex gap-3 items-center'>
+                                <li key={index} className='flex gap-3 items-center max-lg:mb-1'>
                                     <input className='scale-125' type="checkbox" name='' id='' />
                                     {category}
                                 </li>
@@ -77,13 +82,13 @@ const Joblist = () => {
             </div>
 
             {/* job list */}
-            <section className='w-full text-gray-800 max-lg:px-4'>
+            <section className='w-full text-gray-800 max-xl:px-4 max-lg:mt-6'>
                 <h3 className='font-semibold text-3xl mb-4' id='job-list'>Latest Jobs</h3>
                 <p className='mb-8'>Get your desired job from top companies</p>
                 <div className='grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-5 mt-8'>
                     {
                         jobsData.map((job, index) => (
-                            <JobCard key={index} job={job}/>
+                            <JobCard key={index} job={job} />
                         ))
                     }
                 </div>
