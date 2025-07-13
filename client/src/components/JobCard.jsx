d0c55193de972cbb6fa25c5c8db3f276bc681154
Aniketu28 <uagreaniket28@gmail.com>
2025-07-13 19:08:39 +0530

    apply job page ready for merge

--- client/src/components/JobCard.jsx
+++ client/src/components/JobCard.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import { assets, jobsData } from '../assets/assets'
+import {useNavigate} from 'react-router-dom'
 
 const JobCard = ({job}) => {
+  
+   const Navigate = useNavigate();
+   
   return (
       <div className='border border-gray-200 p-6 shadow rounded'>
         <div className='flex justify-between items-center mb-4'>
@@ -14,8 +18,8 @@ const JobCard = ({job}) => {
          </div>
          <p className='text-gray-500 text-base mt-3' dangerouslySetInnerHTML={{__html:job.description.slice(0,150)}}></p>
          <div className='flex items-center gap-4 mt-4 text-sm'>
-            <button className='bg-blue-600 px-4 py-2 text-white rounded'>Apply now</button>
-            <button className='border border-gray-400 px-4 py-2 rounded'>Learn More</button>
+            <button onClick={()=> {Navigate(`/apply-job/${job._id}`); scrollTo(0,0)}} className='cursor-pointer bg-blue-600 px-4 py-2 text-white rounded'>Apply now</button>
+            <button onClick={()=> {Navigate(`/apply-job/${job._id}`); scrollTo(0,0)}} className='cursor-pointer border border-gray-400 px-4 py-2 rounded'>Learn More</button>
          </div>
       </div>
   )
